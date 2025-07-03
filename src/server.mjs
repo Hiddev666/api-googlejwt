c013b9f00e5def01b3c2155ea6c727e3e10550e4
Hiddev666 <officehiddev@gmail.com>
2025-07-03 21:20:50 +0700

    initial commit

---
+++ src/server.mjs
@@ -0,0 +1,77 @@
+import express from "express"
+import session from "express-session"
+import dotenv from "dotenv"
+import jwt from "jsonwebtoken"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import passport from "./auth/google.mjs"
+
+dotenv.config()
+
+const app = express()
+const PORT = 8000
+
+app.use(cookieParser())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}))
+
+app.use(passport.initialize())
+
+// ==== AUTH ROUTES ====
+
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+)
+
+app.get("/auth/google/callback",
+    passport.authenticate("google", { session: false }),
+    (req, res) => {
+        const user = req.user
+        const payload = {
+            id: user.id,
+            displayName: user.displayName,
+            email: user.emails[0].value,
+            profile_picture: user.photos[0].value
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" })
+
+        res.cookie("token", token)
+
+        res.redirect(`${process.env.FRONTEND_URL}/dashboard`)
+    }
+)
+
+app.get("/auth/failure", (req, res) => {
+    res.status(401).send("Auth gagal cuy 😢")
+})
+
+app.get("/api/user", (req, res) => {
+    const bearerToken = req.header("Authorization")
+    const jwtToken = bearerToken.split(" ")[1]
+
+    if (!bearerToken) return res.status(401).json({ message: "No token" })
+
+    try {
+        const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET)
+        res.json(decoded)
+    } catch (err) {
+        res.status(403).json({ message: "Invalid token" })
+    }
+})
+
+app.get("/logout", (req, res) => {
+    res.clearCookie("token")
+    req.logout(() => {
+        res.redirect(`${process.env.FRONTEND_URL}/`)
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server ready di http://localhost:${PORT}`)
+})
+
+export default app
